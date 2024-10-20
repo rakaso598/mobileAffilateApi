3da73e493d18a484aceaf2415db8696dcedfbd53
Micoko <112613372+rakaso598@users.noreply.github.com>
2024-10-20 13:07:18 +0900

    code-clean

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -2,11 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -20,4 +21,5 @@ public class ApiController {
         String data = apiService.getApiData();
         return ResponseEntity.ok(data);
     }
+
 }
